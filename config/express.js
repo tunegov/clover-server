458728d210aa6580b55b35a65a96262714daead0
tunegov <s.e.tunegov@gmail.com>
2018-10-14 15:48:55 +0300

    Fixed routes path

--- config/express.js
+++ config/express.js
@@ -47,7 +47,7 @@ if (config.env === 'development') {
 }
 
 // mount all routes on /api path
-app.use('/api', routes);
+app.use('/v1', routes);
 
 // if error is not an instanceOf APIError, convert it.
 app.use((err, req, res, next) => {
